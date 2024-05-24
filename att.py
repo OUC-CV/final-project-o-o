d58264b351720ece0455a393d2307638f6a3e881
9qxz <103556735+9qxz@users.noreply.github.com>
2024-05-24 20:37:26 +0800

    Update att.py

--- att.py
+++ att.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def hdr_to_brightness(hdr_image):
-    R, G, B = cv2.split(hdr_image)
+    B, G, R = cv2.split(hdr_image)
     brightness = 0.265 * R + 0.670 * G + 0.065 * B
     return brightness
 
@@ -27,7 +27,6 @@ def get_jnd(La):
 def calculate_histogram_2D(brightness_channel, bins_centers):
     histogram = np.zeros(len(bins_centers) - 1)
 
-    # Assign pixels to bins
     for pixel in np.nditer(brightness_channel):
         for i in range(len(bins_centers) - 1):
             if bins_centers[i] <= pixel < bins_centers[i + 1]:
@@ -39,7 +38,6 @@ def calculate_histogram_2D(brightness_channel, bins_centers):
 def calculate_histogram_1D(brightness, bins_centers):
     histogram = np.zeros(len(bins_centers) - 1)
 
-    # Assign pixels to bins
     for pixel in brightness:
         for i in range(len(bins_centers) - 1):
             if bins_centers[i] <= pixel < bins_centers[i + 1]:
@@ -48,7 +46,55 @@ def calculate_histogram_1D(brightness, bins_centers):
 
     return histogram
 
-# Load HDR image
+def normalize_histogram(histogram):
+    total = np.sum(histogram)
+    normalized_histogram = histogram / total
+    return normalized_histogram
+
+def show_histogram(histogram):
+    plt.bar(range(len(bins_centers) - 1), histogram, width=1.0)
+    plt.xlabel('Bin Index')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Brightness Values')
+    plt.show()
+
+def show_histograms(*histograms, labels=None):
+    num_bins = len(histograms[0])
+    bin_indices = np.arange(num_bins)
+    width = 0.8 / len(histograms)  # 设置条形宽度
+
+    # 绘制每个直方图
+    for i, histogram in enumerate(histograms):
+        if labels is not None:
+            label = labels[i]
+        else:
+            label = f'Histogram {i+1}'
+        plt.bar(bin_indices + i * width, histogram, width=width, label=label)
+
+    plt.xlabel('Bin Index')
+    plt.ylabel('Frequency')
+    plt.title('Histograms of Brightness Values')
+    plt.legend()
+    plt.show()
+
+def create_LUT(bins_centers, cumulative_histogram):
+    LUT = np.zeros((len(bins_centers), 2))
+    LUT[:, 0] = bins_centers
+    LUT[:, 1] = cumulative_histogram * 255
+    return LUT
+
+
+def map_hdr_brightness_to_ldr(brightness_channel, LUT):
+    hdr_values = LUT[:, 0]
+    ldr_values = LUT[:, 1]
+
+    ldr_brightness_channel = np.interp(brightness_channel.flatten(), hdr_values, ldr_values)
+    ldr_brightness_channel = ldr_brightness_channel.reshape(brightness_channel.shape)
+    return ldr_brightness_channel
+
+
+
+# 加载图片
 hdr_image = cv2.imread('data/input_images/input_images/input_hdr/AtriumMorning.hdr', cv2.IMREAD_ANYDEPTH)
 
 brightness_channel = hdr_to_brightness(hdr_image)
@@ -61,8 +107,8 @@ if min_brightness == 0:
 print(f"Max brightness: {max_brightness}")
 print(f"Min brightness: {min_brightness}")
 
-# Calculate bins centers
-n = 12.6  # JND step count
+# 我们的bins_centers是bins的边界，和文章中的不同，我们这样做是为了简单，因为文章中只提到了bins的中心，没有提到bins的边界是怎么划分的
+n = 15  # 公式bi+1 = bi + n * delta_b中的n
 bins_centers = [min_brightness]
 
 while bins_centers[-1] < max_brightness:
@@ -71,20 +117,25 @@ while bins_centers[-1] < max_brightness:
     next_bin_center = current_bin_center + n * delta_b
     bins_centers.append(next_bin_center)
 
-# Remove the last bin if it exceeds max_brightness
 if bins_centers[-1] > max_brightness:
     bins_centers.pop()
 
-print(f"Number of bins: {len(bins_centers)}")
-print(f"Bins centers: {bins_centers}")
+print(f"Number of bins: {len(bins_centers) - 1}")
+
+# 每十个 bin 打印成一行
+for i in range(0, len(bins_centers), 10):
+    print("Bins:", end=" ")
+    for j in range(i, min(i + 10, len(bins_centers))):
+        print(f"{bins_centers[j]:.2f}", end=" ")
+    print()
+
 
 f_histogram = calculate_histogram_2D(brightness_channel, bins_centers)
+# 归一化第一个直方图
+f_histogram = normalize_histogram(f_histogram)
+show_histogram(f_histogram)
+
 print(f"Histogram bins: {f_histogram}")
-plt.bar(range(len(bins_centers) - 1), f_histogram, width=1.0)
-plt.xlabel('Bin Index')
-plt.ylabel('Frequency')
-plt.title('Histogram of Brightness Values')
-plt.show()
 
 # Sort brightness values
 sorted_brightness = np.sort(brightness_channel.flatten())
@@ -112,16 +163,35 @@ for i in range(1, len(sorted_brightness)):
         current_brightness = next_brightness
         current_count = 1
 
-# Append the last bin
 new_brightness_levels.append(current_brightness)
 new_brightness_counts.append(current_count)
 
 print(len(new_brightness_levels))
 r_histogram = calculate_histogram_1D(new_brightness_levels, bins_centers)
+r_histogram = normalize_histogram(r_histogram)
 print(f"Histogram bins: {r_histogram}")
 
-plt.bar(range(len(bins_centers) - 1), r_histogram, width=1.0)
-plt.xlabel('Bin Index')
-plt.ylabel('Frequency')
-plt.title('Histogram of Brightness Values')
+w = 0.8
+c_histogram = w * f_histogram + (1 - w) * r_histogram
+print(c_histogram)
+
+# # 绘制三个直方图
+# show_histograms(f_histogram, r_histogram, c_histogram, labels=['Original', 'Refined', 'Combined'])
+cumulative_histogram = np.cumsum(c_histogram)
+cumulative_histogram = np.insert(cumulative_histogram, 0, 0)
+
+
+LUT = create_LUT(bins_centers, cumulative_histogram)
+print("LUT:")
+print(LUT)
+
+
+ldr_brightness_channel = map_hdr_brightness_to_ldr(brightness_channel, LUT)
+ldr_image = np.clip(ldr_brightness_channel, 0, 255).astype(np.uint8)
+
+
+plt.imshow(ldr_image, cmap='gray')
+plt.title('LDR Image (Mapped)')
+plt.axis('off')
+
 plt.show()
