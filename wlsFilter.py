795247251529dc3d621159b33b6517426827af3c
9qxz <103556735+9qxz@users.noreply.github.com>
2024-06-21 09:49:55 +0800

    Delete wlsFilter.py

--- wlsFilter.py
+++
@@ -1,7 +0,0 @@
-import matlab.engine
-
-def wlsFilter(IN, lambda_val=0.5, alpha_val=1.2):
-    eng = matlab.engine.start_matlab()
-    OUT = eng.wlsFilter(matlab.double(IN.tolist()), lambda_val, alpha_val)
-    eng.quit()
-    return OUT
